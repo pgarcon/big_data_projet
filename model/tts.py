0060a3b59e6fa87e83b4b6d17415f6d03860671c
Nicolas720 <98156377+Nicolas720@users.noreply.github.com>
2023-12-06 13:56:13 +0100

    tts

---
+++ model/tts.py
@@ -0,0 +1,22 @@
+# Import
+from gtts import gTTS
+from IPython.display import Audio
+import os
+
+# The text that you want to convert to audio
+mytext = ''
+language = 'fr'
+f = open("tts/tts.txt", encoding="utf-8")
+
+for line in f.readlines():
+    if line != "\n":
+        if "<new>" in line:
+            gTTS(text=mytext, lang=language, slow=False).save("tts/audio.mp3")
+            mytext = ''
+        else:
+            mytext += line
+
+gTTS(text=mytext, lang=language, slow=False).save("tts/audio.mp3")
+f.close()
+
+Audio("audio.mp3", autoplay=True)
