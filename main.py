8c1ff73859e4ec547ec6af50a9e2de041a1c48ab
Pierre Garcon <pierre.garcon@outlook.fr>
2023-12-06 14:17:49 +0100

    ca avance

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from pyspark.sql import SparkSession
 # Define Spark session
 spark = SparkSession.builder \
     .appName("MongoDBExample") \
-    .config("spark.mongodb.input.uri", "mongodb://localhost:27019/yourDatabase.yourCollection") \
-    .config("spark.mongodb.output.uri", "mongodb://localhost:27019/yourDatabase.yourCollection") \
+    .config("spark.mongodb.input.uri", "mongodb://localhost:27019/test.myFirstOne") \
+    .config("spark.mongodb.output.uri", "mongodb://localhost:27019/test.myFirstOne") \
     .getOrCreate()
 
 # Now you can use the 'spark' variable in the rest of your script
